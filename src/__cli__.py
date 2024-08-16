769514158d183fbc61d8f214cf463191fb72220f
distortedlogic <jermeek@gmail.com>
2024-08-16 18:27:38 -0400

    Update settings.json, MANIFEST.in, polar_patch.toml, and 3 more files

--- src/__cli__.py
+++ src/__cli__.py
@@ -1,22 +1,30 @@
 import typer
 
+from src import setup_vscode, mount_plugins, unmount_plugins
+
 app = typer.Typer(name="pp")
 
 
 @app.command()
-def mount(name: str, age: int) -> None:
+def mount() -> None:
   """
   Mount your plugins type hints
   """
-  typer.echo(f"Hello {name}, you are {age} years old!")
+  mount_plugins()
 
 
 @app.command()
-def unmount(name: str, age: int) -> None:
+def unmount() -> None:
   """
   Unmount your plugins type hints
   """
-  typer.echo(f"Hello {name}, you are {age} years old!")
+  unmount_plugins()
+
+
+@app.command()
+def vsc_schema() -> None:
+  """Set up VSCode to use the JSON schema for polar_patch.toml."""
+  setup_vscode()
 
 
 if __name__ == "__main__":
