ccaf436e33fef4cd0800f898139cb0da73c3a80e
distortedlogic <jermeek@gmail.com>
2024-08-19 21:48:21 -0400

    Update README.md, __cli__.py, and patcher.py

--- polar_patch/__cli__.py
+++ polar_patch/__cli__.py
@@ -1,4 +1,9 @@
+import inspect
+import importlib
+from pathlib import Path
+
 import typer
+from polars_classes import POLARS_NAMESPACES
 
 app = typer.Typer(name="pp", pretty_exceptions_show_locals=False, pretty_exceptions_short=True)
 
@@ -20,6 +25,10 @@ def unmount() -> None:
   """
   Unmount your plugins type hints
   """
-  import rich
-
-  rich.print("Please uninstall and reinstall polars with your package manager.")
+  polars_module = importlib.import_module("polars")
+  for ns in POLARS_NAMESPACES:
+    file_path = Path(inspect.getfile(getattr(polars_module, ns)))
+    backup_path = file_path.with_suffix(".bak")
+    if backup_path.is_file():
+      file_path.write_text(backup_path.read_text())
+      backup_path.unlink()
